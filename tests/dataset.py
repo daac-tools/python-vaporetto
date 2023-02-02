e24db0bd4e4fcab46ce45026261b00c707008ca6
Koichi Akabe <vbkaisetsu@gmail.com>
2023-02-02 14:34:00 +0900

    update tests

--- tests/dataset.py
+++ tests/dataset.py
@@ -3,10 +3,10 @@ import os.path
 DIRNAME = os.path.dirname(__file__)
 
 
-def get_kytea_model_path():
+def get_kytea_model_path() -> str:
     return DIRNAME + '/data/jp-0.4.7-5.mod'
 
 
-def load_wagahaiwa_nekodearu():
+def load_wagahaiwa_nekodearu() -> list[str]:
     with open(DIRNAME + '/data/wagahaiwa_nekodearu.txt') as fp:
-        return fp.read()
+        return list(fp)
--- tests/test_comparison_cat.py
+++ tests/test_comparison_cat.py
@@ -1,40 +1,45 @@
 import vaporetto
-import Mykytea
-from sudachipy import tokenizer as sudachi_tokenizer
+import Mykytea  # type: ignore[import]
+from sudachipy import tokenizer as sudachi_tokenizer  # type: ignore[import]
 from sudachipy import dictionary as sudachi_dictionary
 
+from pytest_benchmark import fixture  # type: ignore[import]
 from tests import dataset
 
 
-def kytea_cat_surfaces(tokenizer, corpus):
+def kytea_cat_surfaces(tokenizer: Mykytea.Mykytea, corpus: list[str]) -> int:
     cnt = 0
     for line in corpus:
         cnt += len(' '.join(tokenizer.getWS(line)))
     return cnt
 
 
-def vaporetto_cat_surfaces(tokenizer, corpus):
+def vaporetto_cat_surfaces(tokenizer: vaporetto.Vaporetto, corpus: list[str]) -> int:
     cnt = 0
     for line in corpus:
         cnt += len(' '.join(t.surface() for t in tokenizer.tokenize(line)))
     return cnt
 
 
-def sudachi_cat_surfaces(tokenizer, mode, corpus):
+def sudachi_cat_surfaces(
+    tokenizer: sudachi_tokenizer.Tokenizer,
+    mode: sudachi_tokenizer.Tokenizer.SplitMode,
+    corpus: list[str],
+) -> int:
     cnt = 0
     for line in corpus:
         cnt += len(' '.join(t.surface() for t in tokenizer.tokenize(line, mode)))
     return cnt
 
 
-def test_kytea_cat_surfaces_bench(benchmark):
+def test_kytea_cat_surfaces_bench(benchmark: fixture.BenchmarkFixture) -> None:
     model_path = dataset.get_kytea_model_path()
     corpus = dataset.load_wagahaiwa_nekodearu()
     tokenizer = Mykytea.Mykytea(f'-model {model_path} -notags')
     benchmark(kytea_cat_surfaces, tokenizer, corpus)
 
 
-def test_vaporetto_cat_surfaces_bench(benchmark):
+def test_vaporetto_cat_surfaces_bench(benchmark: fixture.BenchmarkFixture) -> None:
     model_path = dataset.get_kytea_model_path()
     corpus = dataset.load_wagahaiwa_nekodearu()
     with open(model_path, 'rb') as fp:
@@ -43,14 +48,14 @@ def test_vaporetto_cat_surfaces_bench(benchmark):
     benchmark(vaporetto_cat_surfaces, tokenizer, corpus)
 
 
-def test_sudachi_a_cat_surfaces_bench(benchmark):
+def test_sudachi_a_cat_surfaces_bench(benchmark: fixture.BenchmarkFixture) -> None:
     corpus = dataset.load_wagahaiwa_nekodearu()
     tokenizer = sudachi_dictionary.Dictionary().create()
     mode = sudachi_tokenizer.Tokenizer.SplitMode.A
     benchmark(sudachi_cat_surfaces, tokenizer, mode, corpus)
 
 
-def test_sudachi_c_cat_surfaces_bench(benchmark):
+def test_sudachi_c_cat_surfaces_bench(benchmark: fixture.BenchmarkFixture) -> None:
     corpus = dataset.load_wagahaiwa_nekodearu()
     tokenizer = sudachi_dictionary.Dictionary().create()
     mode = sudachi_tokenizer.Tokenizer.SplitMode.C
